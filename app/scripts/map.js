63f66714bc0d23c2200c50744916daabc3c420a1
Jordinaire <jordi.bastide@gmail.com>
2015-04-07 02:00:46 +0200

    add script directly on html file =(

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,145 +1,117 @@
-/*
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById('googleMap'),mapProp);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-*/
-/*global google */
-'use strict';
 
-var map, geocoder;
+    /*global google */
+    'use strict';
 
-var MY_MAPTYPE_ID = 'custom_style';
+    var map;
+    var lekker = new google.maps.LatLng(48.854243, 2.3703059,14);
 
-function initialize() {
+    var MY_MAPTYPE_ID = 'custom_style';
 
-  var featureOpts = 
-  [
-    {
+    function initialize() {
+
+      var featureOpts = 
+      [
+      {
         'featureType': 'administrative',
         'elementType': 'labels.text.fill',
         'stylers': [
-            {
-                'color': '#444444'
-            }
+        {
+            'color': '#444444'
+        }
         ]
     },
     {
         'featureType': 'landscape',
         'elementType': 'all',
         'stylers': [
-            {
-                'color': '#dadee1'
-            }
+        {
+            'color': '#dadee1'
+        }
         ]
     },
     {
         'featureType': 'poi',
         'elementType': 'all',
         'stylers': [
-            {
-                'visibility': 'off'
-            }
+        {
+            'visibility': 'off'
+        }
         ]
     },
     {
         'featureType': 'road',
         'elementType': 'all',
         'stylers': [
-            {
-                'saturation': -100
-            },
-            {
-                'lightness': 45
-            }
+        {
+            'saturation': -100
+        },
+        {
+            'lightness': 45
+        }
         ]
     },
     {
         'featureType': 'road.highway',
         'elementType': 'all',
         'stylers': [
-            {
-                'visibility': 'simplified'
-            }
+        {
+            'visibility': 'simplified'
+        }
         ]
     },
     {
         'featureType': 'road.arterial',
         'elementType': 'labels.icon',
         'stylers': [
-            {
-                'visibility': 'off'
-            }
+        {
+            'visibility': 'off'
+        }
         ]
     },
     {
         'featureType': 'transit',
         'elementType': 'all',
         'stylers': [
-            {
-                'visibility': 'off'
-            }
+        {
+            'visibility': 'off'
+        }
         ]
     },
     {
         'featureType': 'water',
         'elementType': 'all',
         'stylers': [
-            {
-                'color': '#485e69'
-            },
-            {
-                'visibility': 'on'
-            }
+        {
+            'color': '#485e69'
+        },
+        {
+            'visibility': 'on'
+        }
         ]
     }
-];
+    ];
 
-//a6b6bc
+    var mapOptions = {
+        zoom: 15,
+        center: lekker,
+        mapTypeControlOptions: {
+          mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+      },
+      mapTypeId: MY_MAPTYPE_ID
+    };
 
-    lat = 48.854243;
-    lon = 2.3703059,14;
-    var latLng = new google.maps.LatLng(lat, lon);
-    geocoder = new google.maps.Geocoder();
-  var mapOptions = {
-    zoom: 15,
-    center: latLng,
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-    },
-    mapTypeId: MY_MAPTYPE_ID
-  };
-
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+    map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-  codeAddress('8+Rue+de+la+Bastille,+75004+Paris');
-  var styledMapOptions = {
-    name: 'Custom Style'
-  };
 
+    //codeAddress('8+Rue+de+la+Bastille,+75004+Paris');
 
+    var styledMapOptions = {
+        name: 'Custom Style'
+    };
 
-  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+    var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
 
-  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
-}
+    map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+    }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
-/////////////////////////////////////////////////////////////////////////
- function codeAddress(address) {
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      }
-    });
-  }
+    google.maps.event.addDomListener(window, 'load', initialize);
